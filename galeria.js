73e9225e0c17359660b1a83986edbad7401e3a20
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-10-06 10:28:10 -0300

    finalizado

--- galeria.js
+++ galeria.js
@@ -1,15 +1,27 @@
 "use strict"
+
+const limparElemento = (elemento) => {
+    while (elemento.firstChild){
+        elemento.removeChild(elemento.lastChild)
+    }
+}
  
-const imagens = [
-    "./img/iamgens 1.jpg",
-    "./img/magens 2.jpg",
-    "./img/imagens 3.jpg",
-    "./img/imagens 4.jpg",
-    "./img/imagens 5.jpg",
-    "./img/imagens 6.jpg",
-    "./img/imagens 7.jpg",
-    "./img/imagens 8.jpg"
-]
+const pegarImagens = (raca) => fetch(`https://dog.ceo/api/breed/${raca}/images`);
+ 
+const pesquisarImagens = async (evento) => {
+ 
+    if(evento.key === 'Enter') {
+        const raca = evento.target.value;
+        const imagensResponse = await pegarImagens(raca);
+        const imagens = await imagensResponse.json();
+    
+        limparElemento(document.querySelector(".galeria-container"));
+        limparElemento(document.querySelector(".slide-container"));
+        carregarGaleria(imagens.message);
+        carregarSlide(imagens.message);
+    }
+ 
+}
  
 const limparId = (url) => {
     const ultimaBarra = url.lastIndexOf("/") + 1
@@ -49,14 +61,14 @@ const criarSlide = (urlImagem, indice, arr) => {
         <a href="#${idAnterior}" class="icones anterior">&#171;</a>
         <img src="${urlImagem}" alt="">
         <a href="#${idProximo}" class="icones proximo">&#187;</a>
-    </div>
+   </div>
 `
 
-container.appendChild(novoDiv)
+    container.appendChild(novoDiv)
 }
 
 const carregarSlide = (imagens) => imagens.forEach(criarSlide)
 
-// carregarImagens()
-carregarGaleria(imagen)
-carregarSlide(imagen)
+
+document.querySelector(".pesquisa-container input ")
+    .addEventListener("keypress", pesquisarImagens)
