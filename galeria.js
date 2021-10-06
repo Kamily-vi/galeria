636a082b9cf3b52479daac19286de4e8f7f26af4
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-10-06 08:20:09 -0300

    .

--- galeria.js
+++ galeria.js
@@ -1,17 +1,21 @@
 "use strict"
  
 const imagens = [
-    "./img/imagens 1.png",
-    "./img/imagens 4.png",
-    "./img/imagens 6.png",
-    "./img/imagens 8.png",
-    "./img/imagens 2.png",
-    "./img/imagens 3.png",
+    "./img/iamgens 1.jpg",
+    "./img/magens 2.jpg",
+    "./img/imagens 3.jpg",
+    "./img/imagens 4.jpg",
     "./img/imagens 5.jpg",
-    "./img/imagens 7.jpg"
+    "./img/imagens 6.jpg",
+    "./img/imagens 7.jpg",
+    "./img/imagens 8.jpg"
 ]
-
-const limparId = (url) => url.replace("./img/", "").slipt(".")[0].replace(" ", "-")
+ 
+const limparId = (url) => {
+    const ultimaBarra = url.lastIndexOf("/") + 1
+    const ultimoPonto = url.lastIndexOf(".")
+    return url.substring(ultimaBarra,ultimoPonto).replace(" ", "-")
+}
  
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
@@ -24,34 +28,35 @@ const criarItem = (urlImagem) => {
     container.appendChild(novoLink)
 }
  
-
+// const carregarImagens = () => imagens.forEach(criarItem)
 const carregarGaleria = (imagens) => imagens.forEach(criarItem)
-
-const criarSlide = (urlImagem, indice) => {
+ 
+const criarSlide = (urlImagem, indice, arr) => {
     const container = document.querySelector(".slide-container")
     const novoDiv = document.createElement("div")
     novoDiv.classList.add("slide")
     novoDiv.id = limparId(urlImagem)
-
-    const indiceAnterior = indice <= 0 ? arr.length - 1 : indice - 1
+ 
+    const indiceAnterior = indice <= 0 ? arr.length -1 : indice - 1
     const idAnterior = limparId(arr[indiceAnterior])
-
+ 
     const indiceProximo = indice >= arr.length -1 ? 0 : indice + 1
-    const iDProximo = limparId(arr[indiceProximo])
-
+    const idProximo = limparId(arr[indiceProximo])
+ 
     novoDiv.innerHTML = `
     <div class="imagem-container">
-    <a href="" class="icones fechar">&#10006;</a>
-    <a href="${idAnterior}" class="icones anterior">&#171;</a>
-    <img src="${urlImagem}" alt="">
-    <a href="${iDProximo}" class="icones proximo">&#187;</a>
+        <a href="" class="icones fechar">&#10006;</a>
+        <a href="#${idAnterior}" class="icones anterior">&#171;</a>
+        <img src="${urlImagem}" alt="">
+        <a href="#${idProximo}" class="icones proximo">&#187;</a>
     </div>
 `
-    container.appendChild(novoDiv)
 
+container.appendChild(novoDiv)
 }
 
 const carregarSlide = (imagens) => imagens.forEach(criarSlide)
- 
-carregarGaleria(imagens)
-carregarSlide(imagens)
+
+// carregarImagens()
+carregarGaleria(imagen)
+carregarSlide(imagen)
